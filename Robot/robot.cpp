0ffb8010ee55bccc31bc5c938864c0e389dd5824
DDOSilva <iol.davidias@gmail.com>
2024-06-18 13:38:30 -0300

    added robot

---
+++ Robot/robot.cpp
@@ -0,0 +1,81 @@
+#include "robot.h"
+#include <Arduino.h>
+
+Robot::Robot() : frontSensor(FRONT_SENSOR_PIN),
+                    leftSensor(LEFT_SENSOR_PIN),
+                    rightSensor(RIGHT_SENSOR_PIN),
+                    fullLeftSensor(FULL_LEFT_SENSOR_PIN),
+                    fullRightSensor(FULL_RIGHT_SENSOR_PIN),
+                    rightMotor(RIGHT_MOTOR_CHANNEL, RIGHT_MOTOR_IN1_PIN, RIGHT_MOTOR_IN2_PIN, RIGHT_MOTOR_PWM_PIN, RIGHT_MOTOR_SPEED_CONST),
+                    leftMotor(LEFT_MOTOR_CHANNEL, LEFT_MOTOR_IN1_PIN, LEFT_MOTOR_IN2_PIN, LEFT_MOTOR_PWM_PIN, LEFT_MOTOR_SPEED_CONST),
+                    vision(),
+                    autoStrategy(),
+                    uStart(MICRO_START_SIGNAL_PIN)
+{
+    this->robot_state = robotState::AWAITING_START;
+}
+
+void Robot::readSensors() {
+    this-> frontSensor.updateSensor();
+    this-> leftSensor.updateSensor();
+    this-> rightSensor.updateSensor();
+    this-> fullLeftSensor.updateSensor();
+    this-> fullRightSensor.updateSensor();
+}
+
+void Robot::print() {
+    //microstarter
+    Serial.print(this->uStart.uUpdate);
+    Serial.print("\t");
+
+    //sensors
+    Serial.print(this->fullLeftSensor.rawRead);
+    Serial.print("\t");
+    Serial.print(this->leftSensor.rawRead);
+    Serial.print("\t");
+    Serial.print(this->frontSensor.rawRead;
+    Serial.print("\t");
+    Serial.print(this->rightSensor.rawRead);
+    Serial.print("\t");
+    Serial.print(this->fullRightSensor.rawRead);
+    Serial.print("\t");
+}
+
+void Robot::fight () {
+    bool initStratFinished = false;
+
+    switch (this->robot_state) {
+        case robotState::AWAITING_START:
+            if (this->uStart.uState == Start_state::START) {
+                this->robot_state = robotState::INIT_STRAT;
+                this->initStrategy = get_SelectedStrategy(STRATEGY_PIN_A, STRATEGY_PIN_B, STRATEGY_PIN_C);
+            }
+            break;
+        
+        case robotState::INIT_STRAT:
+            initStratFinished = this->initStrategy->update(this->leftMotor, this->rightMotor);
+            if (initStratFinished)
+                this->robot_state = robotState::AUTO_STRAT;
+            if (this->uStart.uState == Start_state::STOP)
+                this->robot_state = robotState::STOPPED;
+            break;
+
+        case robotState::AUTO_STRAT:
+            this->autoStrategy.updateMotor(this->vision, this->leftMotor, this->rightMotor);
+            
+            if(this->uStart.uState == Start_state::STOP)
+                this->robot_state = robotState::STOPPED;
+            break;
+        
+        case robotState::STOPPED:
+            leftMotor.setSpeed(0);
+            rightMotor.setSpeed(0);
+            break;
+
+        default:
+            Serial.println("UNKNOWN BEHAVIOR");
+    }
+}
+
+
+
---
+++ Robot/robot.h
@@ -0,0 +1,79 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+#include <iostream>
+#include <string>
+
+#include "Microstarter.h"
+#include "vision.h"
+#include "DigitalSensor.h"
+#include "Motor.h"
+#include "Strategy.h"
+
+using namespace std;
+
+//defining every pin
+#define FRONT_SENSOR_PIN 35
+#define FULL_RIGHT_SENSOR_PIN 25
+#define FULL_LEFT_SENSOR_PIN 13
+#define LEFT_SENSOR_PIN 26
+#define RIGHT_SENSOR_PIN 34
+
+#define RIGHT_MOTOR_PWM_PIN 21
+#define RIGHT_MOTOR_IN1_PIN 19
+#define RIGHT_MOTOR_IN2_PIN 18
+#define RIGHT_MOTOR_SPEED_CONST 1.0
+#define RIGHT_MOTOR_CHANNEL 0
+
+#define LEFT_MOTOR_PWM_PIN 4
+#define LEFT_MOTOR_IN1_PIN 17
+#define LEFT_MOTOR_IN2_PIN 16
+#define LEFT_MOTOR_SPEED_CONST 1.0
+#define LEFT_MOTOR_CHANNEL 1
+
+#define MICRO_START_SIGNAL_PIN 2
+
+#define STRATEGY_PIN_A 22
+#define STRATEGY_PIN_B 23
+#define STRATEGY_PIN_C 14
+
+#define STDBY 5
+
+namespace robotState {
+    enum states {
+        AWAITING_START,
+        INIT_STRAT,
+        AUTO_STRAT,
+        STOPPED
+    };
+}
+
+class Robot {
+    public:
+        string name = "robot";
+        int ledPin = 13;
+        robotState::states robot_state;
+
+        Microstarter uStart;
+        DigitalSensor frontSensor;
+        DigitalSensor leftSensor;
+        DigitalSensor rightSensor;
+        DigitalSensor fullRightSensor;
+        DigitalSensor fullLeftSensor;
+        Vision vision;
+
+        Motor rightMotor;
+        Motor leftMotor;
+        InitialStrat* initStrategy;
+        AutoStrat autoStrategy;
+
+        void readSensors();
+        void update();
+        void print();
+        void fight();
+
+        Robot();
+
+}
+
+#endif
