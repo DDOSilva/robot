4290b98dfe6a2682851a0393a273cea083121dbe
DDOSilva <iol.davidias@gmail.com>
2024-07-03 12:22:58 -0300

    commented motor

--- Motor/Motor.cpp
+++ Motor/Motor.cpp
@@ -1,33 +1,34 @@
 #include "Motor.h"
 
 MotorConfig::MotorConfig (int pwmPin, int inPin1, int inPin2, int cSpeed, int channelPin) {
-    this->inPin1 = inPin1;
-    this->inPin2 = inPin2;
-    this->pwmPin = pwmPin;
-    this->cSpeed = cSpeed;
-    this->channelPin = channelPin;
+    this->inPin1 = inPin1;              // atributes parameter value to inPin1 
+    this->inPin2 = inPin2;              // atributes parameter value to inPin2
+    this->pwmPin = pwmPin;              // atributes parameter value to pwmPin
+    this->cSpeed = cSpeed;              // atributes parameter value to cSpeed
+    this->channelPin = channelPin;      // atributes parameter value to channelPin
 }
 
 Motor::Motor (int pwmPin, int inPin1, int inPin2, int cSpeed, int channelPin) {}
 
-//should be between -100 and 100
-void Motor::setSpeed(int speed) {
-    this->speed = speed;
-    float fixSpeed = this->config.cSpeed;
+    // defines motor's power (speed) and rotation's direction (clockwiser/counterclockwise)
+    // speed value needs to be in a -100 to 100 range
+    void Motor::setSpeed(int speed) {
+        this->speed = speed;                        // atributes parameter value to speed
+        float fixSpeed = this->config.cSpeed;       // creates fixSpeed with cSpeed value (defined as 1.0 in robot.cpp). will be used in convertion to [-255,255]
 
-    //convertion to [-255, 255] range
-    int pwm = (int)(this->maxSpeed / (100.0) * 255.0 *((float)abs(fixSpeed) / 100);
+        // converts pwm value from a -100 to 100 range to a -255 to 255 range.
+        int pwm = (int)(this->maxSpeed / (100.0) * 255.0 *((float)abs(fixSpeed) / 100);
 
-    //conditions
-    if (fixSpeed < 0 && fixSpeed >= -100) {
-        digitalWrite(this->config.inPin1, 1);
-        digitalWrite(this->config.inPin2, 0);
-    } else if (fixSpeed >= 0 && fixSpeed <= 100) {
-        digitalWrite(this->config.inPin1, 0);
-        digitalWrite(this->config.inPin2, 1);
-    } else {
-        Serial.println("INVALID SPEED VALUE");
-        return;
-    }
-    ledcWrite(this->config.channelPin, pwm);
+        // conditions that will check rotation's direction 
+        if (fixSpeed < 0 && fixSpeed >= -100) {             // if value is negative, rotation will be counterclockwise
+            digitalWrite(this->config.inPin1, 1);
+            digitalWrite(this->config.inPin2, 0);           // this pin configuration will define the direction
+        } else if (fixSpeed >= 0 && fixSpeed <= 100) {      // if value is positive, rotation will be clockwise
+            digitalWrite(this->config.inPin1, 0);
+            digitalWrite(this->config.inPin2, 1);           // this pin configuration will define the direction
+        } else {
+            Serial.println("INVALID SPEED VALUE");      // error
+            return;
+        }
+        ledcWrite(this->config.channelPin, pwm);        // writes pwm value on channelPin
 }
--- Motor/Motor.h
+++ Motor/Motor.h
@@ -1,21 +1,22 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
+// struct that will store important info like pins and so on
 struct MotorConfig {
     MotorConfig(int pwmPin, int inPin1, int inPin2, int cSpeed, int channelPin);
-    int inPin1;
-    int inPin2;
-    int pwmPin;
-    float cSpeed;
-    int channelPin;
+    int inPin1;             // used to determine if motor's rotation will be clockwise or counterclockwise
+    int inPin2;             // used to determine if motor's rotation will be clockwise or counterclockwise
+    int pwmPin;             // controls motor's power (speed)
+    float cSpeed;           // used to control motor's power (speed)
+    int channelPin;         // sends signal to esp
 }
 
 class Motor {
     public:
         Motor(int pwmPin, int inPin1, int inPin2, int cSpeed, int channelPin);
-        float maxSpeed = 100;
-        int speed;
-        MotorConfig config;
-        void setSpeed(int speed);
+        float maxSpeed = 100;           // max power that the motor can reach (100%)
+        int speed;                      // receives power (speed) from pwm
+        MotorConfig config;             // accesses info from struct
+        void setSpeed(int speed);       // set motor's desired power (speed), using a -100 to 100 range
 };
 #endif
