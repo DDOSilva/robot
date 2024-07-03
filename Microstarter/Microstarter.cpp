c7a6105e97a23afb502021ff7bccab08664d803b
DDOSilva <iol.davidias@gmail.com>
2024-07-03 11:06:45 -0300

    commented microstarter

--- Microstarter/Microstarter.cpp
+++ Microstarter/Microstarter.cpp
@@ -2,15 +2,16 @@
 #include <Arduino.h>
 
 Microstarter::Microstarter(int uPin) {
-    this->uPin = uPin;
-    this->uState = Start_state::STOP;
-}
+    this->uPin = uPin;                          // atributes parameter value to uPin
+    this->uState = Start_state::STOP;           // defines microstarter state as STOP so the robot will only move if it receives signal from IR module
+};
 
 void Microstarter::uUpdate() {
-    bool readPin = digitalRead(this->uPin);
+    bool readPin = digitalRead(this->uPin);      // reads signal received from uPin
+
     if (readPin == true) {
-        this->uState = Start_state::START;
+        this->uState = Start_state::START;      // if uPin receives signal, changes uState to START, causing the robot to move
     } else {
-        this->uState = Start_state::STOP;
+        this->uState = Start_state::STOP;       // if uPin doesn't receive signal, keeps uState as STOP
     } 
-}
+};
--- Microstarter/Microstarter.h
+++ Microstarter/Microstarter.h
@@ -1,18 +1,20 @@
 #ifndef MICROSTARTER_H
 #define MICROSTARTER_H
 
+//creates integer constants 
 namespace Start_state {
     enum states {
-        START,
-        STOP
+        START,              // 0
+        STOP                // 1
     };
 }
 
 class Microstarter {
     public:
-        Microstarter(int uPin);
-        int uPin;
-        Start_state::states uState;
-        void uUpdate();
+        Microstarter(int uPin); 
+
+        int uPin;                        // pin that receives signal from IR module
+        Start_state::states uState;      // stores received signal state
+        void uUpdate();                  // read signal from pin and sends to states
 };
 #endif
